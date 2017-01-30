b816da79a82155b6e2405310373e9425d2674907
eugeneat@yandex.ru <eugeneath@yandex.ru>
2017-01-30 15:27:55 +0700

    string-fix, eslint-fix

--- js/form.js
+++ js/form.js
@@ -5,12 +5,12 @@ var dialog = document.querySelector('.dialog');
 var dialogClose = dialog.querySelector('.dialog__close');
 
 var deactivatePins = function () {
-  for (var i = 0; i < pin.length; i++){
+  for (var i = 0; i < pin.length; i++) {
     pin[i].classList.remove('pin--active');
   }
 };
 
-for (var i = 0; i < pin.length; i++){
+for (var i = 0; i < pin.length; i++) {
   pin[i].addEventListener('click', function () {
     deactivatePins();
     this.classList.add('pin--active');
@@ -27,27 +27,26 @@ var formTime = document.querySelector('#time');
 var formTimeout = document.querySelector('#timeout');
 
 formTime.addEventListener('change', function () {
-  formTimeout.selectedIndex = this.selectedIndex;
+  formTimeout.selectedIndex = formTime.selectedIndex;
 });
 
 formTimeout.addEventListener('change', function () {
-  formTime.selectedIndex = this.selectedIndex;
+  formTime.selectedIndex = formTimeout.selectedIndex;
 });
 
 var formType = document.querySelector('#type');
 var formPrice = document.querySelector('#price');
 
 formType.addEventListener('change', function () {
-  if (this.value === 'flat') {
+  if (formType.value === 'flat') {
     formPrice.setAttribute('min', '1000');
-  } else if (this.value === 'shack') {
+  } else if (formType.value === 'shack') {
     formPrice.setAttribute('min', '0');
-  } else if (this.value === 'palace') {
+  } else if (formType.value === 'palace') {
     formPrice.setAttribute('min', '10000');
   }
 
-  /* Почему не работает это обнуление? */
-  if (formPrice.value < formPrice.min) {
+  if (parseInt(formPrice.value, 10) < parseInt(formPrice.min, 10)) {
     formPrice.value = '';
   }
 });
@@ -56,17 +55,17 @@ var formRoomNumber = document.querySelector('#room_number');
 var formCapacity = document.querySelector('#capacity');
 
 formRoomNumber.addEventListener('change', function () {
-  if (this.value === '1') {
+  if (formRoomNumber.value === '1') {
     formCapacity.value = '0';
-  } else if (this.value === '2' || this.value === '100') {
+  } else if (formRoomNumber.value === '2' || formRoomNumber.value === '100') {
     formCapacity.value = '3';
   }
 });
 
 formCapacity.addEventListener('change', function () {
-  if (this.value === '0') {
+  if (formCapacity.value === '0') {
     formRoomNumber.value = '1';
-  } else if (this.value === '3') {
+  } else if (formCapacity.value === '3') {
     formRoomNumber.value = '2';
   }
 });
