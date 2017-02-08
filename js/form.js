19095dbc0f09b14a49f4ce650c4fb426581f6aa8
eugeneat@yandex.ru <eugeneath@yandex.ru>
2017-02-08 10:25:33 +0700

    arrays to vars

--- js/form.js
+++ js/form.js
@@ -4,23 +4,25 @@ var pinMap = document.querySelector('.tokyo__pin-map');
 var pins = pinMap.querySelectorAll('.pin');
 var dialog = document.querySelector('.dialog');
 var dialogClose = dialog.querySelector('.dialog__close');
-
 window.initializePins(pinMap, pins, dialog, dialogClose);
 
 var formTime = document.querySelector('#time');
 var formTimeout = document.querySelector('#timeout');
-
-window.synchronizeFields(formTime, formTimeout, ['12', '13', '14'], ['12', '13', '14'], 'value');
-window.synchronizeFields(formTimeout, formTime, ['12', '13', '14'], ['12', '13', '14'], 'value');
+var formTimeValues = ['12', '13', '14'];
+var formTimeoutValues = ['12', '13', '14'];
+window.synchronizeFields(formTime, formTimeout, formTimeValues, formTimeoutValues, 'value');
+window.synchronizeFields(formTimeout, formTime, formTimeoutValues, formTimeValues, 'value');
 
 var formType = document.querySelector('#type');
 var formPrice = document.querySelector('#price');
-
-window.synchronizeFields(formType, formPrice, ['flat', 'shack', 'palace'], ['1000', '0', '10000'], 'min');
+var formTypeValues = ['flat', 'shack', 'palace'];
+var formPriceValues = ['1000', '0', '10000'];
+window.synchronizeFields(formType, formPrice, formTypeValues, formPriceValues, 'min');
 
 var formRoomNumber = document.querySelector('#room_number');
 var formCapacity = document.querySelector('#capacity');
-
-window.synchronizeFields(formRoomNumber, formCapacity, ['1', '2', '100'], ['0', '3', '3'], 'value');
-window.synchronizeFields(formCapacity, formRoomNumber, ['0', '3', '3'], ['1', '2', '100'], 'value');
+var formRoomNumberValues = ['1', '2', '100'];
+var formCapacityValues = ['0', '3', '3'];
+window.synchronizeFields(formRoomNumber, formCapacity, formRoomNumberValues, formCapacityValues, 'value');
+window.synchronizeFields(formCapacity, formRoomNumber, formCapacityValues, formRoomNumberValues, 'value');
 
