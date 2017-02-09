2125ed2f7c869922b73b42a58a509d15efbf892b
eugeneat@yandex.ru <eugeneath@yandex.ru>
2017-02-09 15:21:25 +0700

    arrays to vars

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,72 +1,73 @@
 'use strict';
+(function () {
+  window.initializePins = function (pinMap, pins, dialog, dialogClose) {
 
-window.initializePins = function (pinMap, pins, dialog, dialogClose) {
-
-  var deactivatePins = function () {
-    for (var i = 0; i < pins.length; i++) {
-      pins[i].classList.remove('pin--active');
-      pins[i].setAttribute('aria-pressed', 'false');
+    var deactivatePins = function () {
+      for (var i = 0; i < pins.length; i++) {
+        pins[i].classList.remove('pin--active');
+        pins[i].setAttribute('aria-pressed', 'false');
+      }
+    };
+  
+    var activatePin = function (clickedPin) {
+      clickedPin.classList.add('pin--active');
+      clickedPin.setAttribute('aria-pressed', 'true');
+    };
+  
+    var openDialog = function () {
+      dialog.style.display = 'block';
+      dialogClose.setAttribute('aria-pressed', 'false');
+      window.addEventListener('keydown', dialogKeydownHandler);
+    };
+  
+    var closeDialog = function () {
+      dialog.style.display = 'none';
+      dialogClose.setAttribute('aria-pressed', 'true');
+      window.removeEventListener('keydown', dialogKeydownHandler);
+    };
+  
+    var ENTER_KEY_CODE = 13;
+    var ESCAPE_KEY_CODE = 27;
+  
+    var showLodgeInfo = function (clickedPin) {
+      deactivatePins(pins);
+      activatePin(clickedPin);
+      openDialog(dialog, dialogClose);
+    };
+  
+    var hideLodgeInfo = function () {
+      deactivatePins(pins);
+      closeDialog(dialog, dialogClose);
+    };
+  
+    var dialogKeydownHandler = function (evt) {
+      if (evt.keyCode === ESCAPE_KEY_CODE) {
+        hideLodgeInfo();
+      }
+    };
+  
+    if (dialog.style.display !== 'none') {
+      window.addEventListener('keydown', dialogKeydownHandler);
     }
+  
+    var pinClickHandler = function (e) {
+      var clickedPin;
+      if (e.target.classList.contains('pin')) {
+        clickedPin = e.target;
+      } else {
+        clickedPin = e.target.parentNode;
+      }
+      showLodgeInfo(clickedPin);
+    };
+  
+    pinMap.addEventListener('click', pinClickHandler);
+  
+    pinMap.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === ENTER_KEY_CODE) {
+        showLodgeInfo(evt.currentTarget);
+      }
+    });
+  
+    dialogClose.addEventListener('click', hideLodgeInfo);
   };
-
-  var activatePin = function (clickedPin) {
-    clickedPin.classList.add('pin--active');
-    clickedPin.setAttribute('aria-pressed', 'true');
-  };
-
-  var openDialog = function () {
-    dialog.style.display = 'block';
-    dialogClose.setAttribute('aria-pressed', 'false');
-    window.addEventListener('keydown', dialogKeydownHandler);
-  };
-
-  var closeDialog = function () {
-    dialog.style.display = 'none';
-    dialogClose.setAttribute('aria-pressed', 'true');
-    window.removeEventListener('keydown', dialogKeydownHandler);
-  };
-
-  var ENTER_KEY_CODE = 13;
-  var ESCAPE_KEY_CODE = 27;
-
-  var showLodgeInfo = function (clickedPin) {
-    deactivatePins(pins);
-    activatePin(clickedPin);
-    openDialog(dialog, dialogClose);
-  };
-
-  var hideLodgeInfo = function () {
-    deactivatePins(pins);
-    closeDialog(dialog, dialogClose);
-  };
-
-  var dialogKeydownHandler = function (evt) {
-    if (evt.keyCode === ESCAPE_KEY_CODE) {
-      hideLodgeInfo();
-    }
-  };
-
-  if (dialog.style.display !== 'none') {
-    window.addEventListener('keydown', dialogKeydownHandler);
-  }
-
-  var pinClickHandler = function (e) {
-    var clickedPin;
-    if (e.target.classList.contains('pin')) {
-      clickedPin = e.target;
-    } else {
-      clickedPin = e.target.parentNode;
-    }
-    showLodgeInfo(clickedPin);
-  };
-
-  pinMap.addEventListener('click', pinClickHandler);
-
-  pinMap.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ENTER_KEY_CODE) {
-      showLodgeInfo(evt.currentTarget);
-    }
-  });
-
-  dialogClose.addEventListener('click', hideLodgeInfo);
-};
+})();
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,7 +1,8 @@
 'use strict';
-
-window.synchronizeFields = function (fieldIn, fieldOut, arrayIn, arrayOut, valueOut) {
-  fieldIn.addEventListener('change', function () {
-    fieldOut[valueOut] = arrayOut[arrayIn.indexOf(fieldIn.value)];
-  });
-};
+(function () {
+  window.synchronizeFields = function (fieldIn, fieldOut, arrayIn, arrayOut, valueOut) {
+    fieldIn.addEventListener('change', function () {
+      fieldOut[valueOut] = arrayOut[arrayIn.indexOf(fieldIn.value)];
+    });
+  };
+})();
