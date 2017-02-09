603a3d542208693b5e382b91bb4a0cbc92a73e2a
eugeneat@yandex.ru <eugeneath@yandex.ru>
2017-02-09 15:28:27 +0700

    trailing spaces

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -8,48 +8,48 @@
         pins[i].setAttribute('aria-pressed', 'false');
       }
     };
-  
+
     var activatePin = function (clickedPin) {
       clickedPin.classList.add('pin--active');
       clickedPin.setAttribute('aria-pressed', 'true');
     };
-  
+
     var openDialog = function () {
       dialog.style.display = 'block';
       dialogClose.setAttribute('aria-pressed', 'false');
       window.addEventListener('keydown', dialogKeydownHandler);
     };
-  
+
     var closeDialog = function () {
       dialog.style.display = 'none';
       dialogClose.setAttribute('aria-pressed', 'true');
       window.removeEventListener('keydown', dialogKeydownHandler);
     };
-  
+
     var ENTER_KEY_CODE = 13;
     var ESCAPE_KEY_CODE = 27;
-  
+
     var showLodgeInfo = function (clickedPin) {
       deactivatePins(pins);
       activatePin(clickedPin);
       openDialog(dialog, dialogClose);
     };
-  
+
     var hideLodgeInfo = function () {
       deactivatePins(pins);
       closeDialog(dialog, dialogClose);
     };
-  
+
     var dialogKeydownHandler = function (evt) {
       if (evt.keyCode === ESCAPE_KEY_CODE) {
         hideLodgeInfo();
       }
     };
-  
+
     if (dialog.style.display !== 'none') {
       window.addEventListener('keydown', dialogKeydownHandler);
     }
-  
+
     var pinClickHandler = function (e) {
       var clickedPin;
       if (e.target.classList.contains('pin')) {
@@ -59,15 +59,14 @@
       }
       showLodgeInfo(clickedPin);
     };
-  
+
     pinMap.addEventListener('click', pinClickHandler);
-  
     pinMap.addEventListener('keydown', function (evt) {
       if (evt.keyCode === ENTER_KEY_CODE) {
         showLodgeInfo(evt.currentTarget);
       }
     });
-  
+
     dialogClose.addEventListener('click', hideLodgeInfo);
   };
 })();
