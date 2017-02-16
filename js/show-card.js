73c8277f16f4b94b4b3cd841550f49dc714a770b
eugeneat@yandex.ru <eugeneath@yandex.ru>
2017-02-16 20:54:57 +0700

    callback in show-card. understanding

--- js/show-card.js
+++ js/show-card.js
@@ -1,19 +1,31 @@
 'use strict';
 (function () {
+  var ENTER_KEY_CODE = 13;
+  var ESCAPE_KEY_CODE = 27;
+  
   var dialog = document.querySelector('.dialog');
   var dialogClose = dialog.querySelector('.dialog__close');
 
-  var ESCAPE_KEY_CODE = 27;
+  window.showCard = function (closeHandler) {             // сначала смотрим строку 23, где движуха
+    var onKeyDown = function (e) {                        // а тут смотрим, что же нажалось
+                                                          // ниже хитрое условие: либо esc, либо enter по крестику
+      if ((e.target === dialogClose && e.keyCode === ENTER_KEY_CODE) || e.keyCode === ESCAPE_KEY_CODE) {
+        hideCard();                                       // и если это так, то вызываем функцию прятания
+      }
+    };
 
-  window.openDialog = function (hideLodgeInfo) {
-    dialog.style.display = 'block';
+    var hideCard = function() {                           // вот и функция прятания
+      dialog.style.display = 'none';                      // скрываем диалог
+      dialogClose.removeEventListener('click', hideCard); // не ждём кликов по крестику
+      window.removeEventListener('keydown', onKeyDown);   // или чего-то с клавиатуры
+      closeHandler();                                     // а вот он и коллбек, благодаря ему мы уже и будем прятать пины, я понял!
+    };
+    
+    dialog.style.display = 'block';                       // показали сам диалог
+    dialogClose.addEventListener('click', hideCard);      // теперь можем ожидать клика на крестик, что вызывает прятание
+    window.addEventListener('keydown', onKeyDown);        // или что-то с клавиатуры (идём на строку 9)
+
     dialogClose.setAttribute('aria-pressed', 'false');
-    window.addEventListener('keydown', function (evt) {
-        if (evt.keyCode === ESCAPE_KEY_CODE) {
-          hideLodgeInfo();
-        }
-    });
-
-    dialogClose.addEventListener('click', hideLodgeInfo);
+    
   };
 })();
