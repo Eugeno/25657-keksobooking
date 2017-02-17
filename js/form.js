3766df8961e696b2bb1a7d60f3bf2a6133f9c381
eugeneat@yandex.ru <eugeneath@yandex.ru>
2017-02-17 20:10:38 +0700

    fix synchronize

--- js/form.js
+++ js/form.js
@@ -8,10 +8,10 @@ var formTime = document.querySelector('#time');
 var formTimeout = document.querySelector('#timeout');
 var formTimeValues = ['12', '13', '14'];
 var formTimeoutValues = ['12', '13', '14'];
-window.synchronizeFields(formTime, formTimeout, formTimeValues, formTimeoutValues, function(newValue) {
+window.synchronizeFields(formTime, formTimeValues, formTimeoutValues, function(newValue) {
   formTimeout.value = newValue
 });
-window.synchronizeFields(formTimeout, formTime, formTimeoutValues, formTimeValues, function(newValue) {
+window.synchronizeFields(formTimeout, formTimeoutValues, formTimeValues, function(newValue) {
   formTime.value = newValue;
 });
 
@@ -19,7 +19,7 @@ var formType = document.querySelector('#type');
 var formPrice = document.querySelector('#price');
 var formTypeValues = ['flat', 'shack', 'palace'];
 var formPriceValues = ['1000', '0', '10000'];
-window.synchronizeFields(formType, formPrice, formTypeValues, formPriceValues, function(newValue) {
+window.synchronizeFields(formType, formTypeValues, formPriceValues, function(newValue) {
   formPrice.min = newValue;
 });
 
@@ -27,9 +27,9 @@ var formRoomNumber = document.querySelector('#room_number');
 var formCapacity = document.querySelector('#capacity');
 var formRoomNumberValues = ['1', '2', '100'];
 var formCapacityValues = ['0', '3', '3'];
-window.synchronizeFields(formRoomNumber, formCapacity, formRoomNumberValues, formCapacityValues, function(newValue) {
+window.synchronizeFields(formRoomNumber, formRoomNumberValues, formCapacityValues, function(newValue) {
   formCapacity.value = newValue;
 });
-window.synchronizeFields(formCapacity, formRoomNumber, formCapacityValues, formRoomNumberValues, function(newValue) {
+window.synchronizeFields(formCapacity, formCapacityValues, formRoomNumberValues, function(newValue) {
   formRoomNumber.value = newValue;
 });
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,6 +1,6 @@
 'use strict';
 (function () {
-  window.synchronizeFields = function (fieldIn, fieldOut, arrayIn, arrayOut, callback) {
+  window.synchronizeFields = function (fieldIn, arrayIn, arrayOut, callback) {
     fieldIn.addEventListener('change', function () {
       callback(arrayOut[arrayIn.indexOf(fieldIn.value)]);
     });
